125fe3825864a6a01694a7a5c2ae07e2b301a0dc
ORADEV\shubhasr <shubham.ss.srivastava@oracle.com>
2021-02-18 17:48:07 +0530

    Basic node server with react ready to be deployed

--- app.js
+++ app.js
@@ -19,6 +19,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Linking app to show react frontend files
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
